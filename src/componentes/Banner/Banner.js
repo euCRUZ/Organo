75be8a1b428a0b37bb31d3f1871810a7efab89da
Giovanni <giovannicruzvinhedo@gmail.com>
2024-04-26 21:48:28 -0300

    #

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner Principal do Organo"></img>
+        </header>
+    )
+}
+
+export default Banner
